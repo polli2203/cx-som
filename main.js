ea32dbf7d39e39eacae86d979da2719504c576fe
polli2203 <146850815+polli2203@users.noreply.github.com>
2023-11-29 13:06:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-function tocaSomPom() 
-{
-    document.querySelector('#som_tecla_pom').play();
+//Criando/declarando a função tocaSom com parâmetro idElementoAudio
+function tocaSom(idElementoAudio){
+    //if else elemento operadores comparação
+    const elemento = document.querySelector(idElementoAudio);
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log("elemento não encontrado");
+    }
+}
+//criando referência constante listaDeTeclas buscando 
+//todos os itens pela classe .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//estrutura de repetição "while" significa 
+//"enquanto algo acontecer" mudou while para "for"
+for(let contador = 0;contador<listaDeTeclas.length;contador++){
+    const tecla = listaDeTeclas [contador];
+    const efeito = tecla.classList [1];
+    const idAudio = #som_${efeito};
+    tecla.onclick = function(){
+        tocaSom (idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
